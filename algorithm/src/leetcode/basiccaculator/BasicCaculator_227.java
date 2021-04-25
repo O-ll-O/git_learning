b6e296676f4b33d5f9ce667572f34c8bb2e71196
xiaolin.zhang(张晓琳) <xiaolin.zhang@advantech.com.cn>
2021-04-25 17:57:25 +0800

    Add 227

--- algorithm/src/leetcode/basiccaculator/BasicCaculator_227.java
+++ algorithm/src/leetcode/basiccaculator/BasicCaculator_227.java
@@ -1,7 +1,46 @@
 package leetcode.basiccaculator;
 
+import java.util.Deque;
+import java.util.LinkedList;
+
 public class BasicCaculator_227 {
 	public static void main(String[] args) {
-
+		String s = "3+2*2";
+		System.out.print(calculate(s));
 	}
+
+	public static int calculate(String s) {
+		Deque<Integer> stack = new LinkedList<Integer>();
+		char preSign = '+';
+		int num = 0;
+		int n = s.length();
+		for (int i = 0; i < n; ++i) {
+			if (Character.isDigit(s.charAt(i))) {
+				num = num * 10 + s.charAt(i) - '0';
+			}
+			if (!Character.isDigit(s.charAt(i)) && s.charAt(i) != ' ' || i == n - 1) {
+				switch (preSign) {
+				case '+':
+					stack.push(num);
+					break;
+				case '-':
+					stack.push(-num);
+					break;
+				case '*':
+					stack.push(stack.pop() * num);
+					break;
+				default:
+					stack.push(stack.pop() / num);
+				}
+				preSign = s.charAt(i);
+				num = 0;
+			}
+		}
+		int ans = 0;
+		while (!stack.isEmpty()) {
+			ans += stack.pop();
+		}
+		return ans;
+	}
+
 }

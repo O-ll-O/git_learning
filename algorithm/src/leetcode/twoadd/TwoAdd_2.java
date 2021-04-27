f921f04aae53da9b219bab3914451e5bc5684f7e
xiaolin.zhang(张晓琳) <xiaolin.zhang@advantech.com.cn>
2021-04-27 16:19:48 +0800

    add

--- algorithm/src/leetcode/twoadd/TwoAdd_2.java
+++ algorithm/src/leetcode/twoadd/TwoAdd_2.java
@@ -9,13 +9,13 @@ public class TwoAdd_2 {
 		ListNode head = null, tail = null;
 		int carry = 0;
 		while (l1 != null || l2 != null) {
-			int n1 = l1 == null ? 0 : l1.val;
-			int n2 = l2 == null ? 0 : l2.val;
+			int n1 = l1 != null ? l1.val : 0;
+			int n2 = l2 != null ? l2.val : 0;
 			int sum = n1 + n2 + carry;
 			if (head == null) {
 				head = tail = new ListNode(sum % 10);
 			} else {
-				tail = new ListNode(sum % 10);
+				tail.next = new ListNode(sum % 10);
 				tail = tail.next;
 			}
 			carry = sum / 10;
